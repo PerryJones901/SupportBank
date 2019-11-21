50932b3911b75da7953273f72787253a36ad1853
PerryJones901 <Perry.Jones@softwire.com>
2019-11-21 14:16:37 +0000

    Update 2

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -2,31 +2,77 @@ package training.supportbank;
 
 import java.io.*;
 import java.lang.reflect.Array;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         //Part 1: Reading CSV Files:
         //  List of Transactions
-        var listOfTransactions = new LinkedList<Array>();
+        var listOfTransactions = new ArrayList<String[]>();
 
         //  Reading File and Adding to list
         FileInputStream fstream = new FileInputStream("Transactions2014.txt");
         BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
         String strLine;
+        int count = 0;
         while ((strLine = br.readLine()) != null)   {
-            //Handle string.
+            listOfTransactions.add(strLine.split(","));
+            System.out.println(listOfTransactions.get(count)[0]);
+            count++;
         }
 
-        //  HashMaps
-        var accounts = new HashMap<String, Integer>();
-        var transactions = new HashMap<String, Integer>();
+        listAcc("Todd",listOfTransactions);
+        listAll(listOfTransactions);
+    }
 
+    //List[Account]:
+    private static void listAcc(String accountName, ArrayList<String[]> listOfTransactions){
+        for(int i = 1; i < listOfTransactions.size(); i++) {
 
+            if (listOfTransactions.get(i)[1].equals(accountName)) {
+                //Paying Others.
+                System.out.println(accountName +
+                        " paid " + listOfTransactions.get(i)[2] + " £" +
+                        listOfTransactions.get(i)[4] + " for " + listOfTransactions.get(i)[3] +
+                        " on " + listOfTransactions.get(i)[0]);
+            } else if(listOfTransactions.get(i)[2].equals(accountName)){
+                //Getting Paid.
+                System.out.println(accountName +
+                        " was paid by " + listOfTransactions.get(i)[1] + " £" +
+                        listOfTransactions.get(i)[4] + " for " + listOfTransactions.get(i)[3] +
+                        " on " + listOfTransactions.get(i)[0]);
+            }
+        }
+    }
+    //List All:
+    private static void listAll(ArrayList<String[]> listOfTransactions){
+        //Make a HashMap of Accounts
 
-        System.out.println("Test!");
+        var hashMapAccs = new HashMap<String, BigDecimal>();
+        for(int i = 1; i < listOfTransactions.size(); i++){
+            var fromName = listOfTransactions.get(i)[1];
+            var toName = listOfTransactions.get(i)[2];
+            var money = new BigDecimal(listOfTransactions.get(i)[4]);
+            //Initialise (if need be):
+            hashMapAccs.putIfAbsent(fromName, new BigDecimal(0));
+            hashMapAccs.putIfAbsent(toName, new BigDecimal(0));
+            //Do the maths:
+            hashMapAccs.put(fromName, hashMapAccs.get(fromName).subtract(money));
+            hashMapAccs.put(toName, hashMapAccs.get(toName).add(money));
+        }
+        //Now iterate through the HashMap.
+        // Get a set of the entries
+        Set set = hashMapAccs.entrySet();
+        // Get an iterator
+        Iterator i = set.iterator();
+
+        // Display elements
+        while(i.hasNext()) {
+            Map.Entry me = (Map.Entry)i.next();
+            System.out.print(me.getKey() + ": " + me.getValue() + "\n");
+        }
     }
 }
